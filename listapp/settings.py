bd89f868f6980247d407f171e55305f94d24657a
lmadkins <louise.m.adkins@gmail.com>
2022-08-24 13:48:47 -0700

    Login/logout nav templating

--- listapp/settings.py
+++ listapp/settings.py
@@ -133,6 +133,8 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR,'main_app/static')]
 
 LOGIN_REDIRECT_URL = '/lists/'
 
+LOGOUT_REDIRECT_URL = '/'
+
 # Configure Django App for Heroku.
 import django_on_heroku
-django_on_heroku.settings(locals())gi
+django_on_heroku.settings(locals())
