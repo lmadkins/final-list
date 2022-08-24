afd774161853545da318c72d5ed443c35c45ffa8
lmadkins <louise.m.adkins@gmail.com>
2022-08-24 15:17:02 -0700

    Authorization setup complete

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from datetime import date
+from django.urls import reverse
 
 LISTTYPES = (
     ('I', 'Items'),
--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,7 @@ from . import views
 # this like app.use() in express
 urlpatterns = [
     path('', views.Home.as_view(), name="home"),
-    path('lists/', views.Lists.as_view(), name="lists_list")
+    path('accounts/signup/', views.Signup.as_view(), name="signup"),
+    path('lists/', views.Lists.as_view(), name="lists_list"),
+    path('lists/new', views.ListCreate.as_view(), name="list_create"),
 ]
